77eb753cfdbfd5babc2b716d388c893a9cba129e
Gabriel Rosa <grosa.ict@gmail.com>
2021-08-30 21:58:53 -0300

    DB configs done

--- apitest.js
+++ apitest.js
@@ -1,13 +1,29 @@
-const express   = require('express')
+const express   = require('express');
+const dotenv    = require('dotenv');
+const morgan    = require('morgan');
 const cors      = require('cors');
+const mongoose  = require('mongoose');
 
-const app       = express()
-const port      = 3000
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: true } ));
+app.use(cors())
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const port = process.env.PORT;
+
+app.listen(port, function (err) {
+    console.log("apitest.js >>>")
+    console.log('server listening on port '+port);
+    if (err) {
+      console.log("err >>>")
+      console.log(err)
+    }
+  });
